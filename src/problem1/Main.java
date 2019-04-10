291b824271d48ee44b212a776f6f67e535321099
DevonCJ <47532377+DevonCJ@users.noreply.github.com>
2019-04-10 11:12:54 -0400

    Project

--- src/problem1/Main.java
+++ src/problem1/Main.java
@@ -5,6 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		// Read a value. Print out from 1 to the input
 		Scanner in=new Scanner(System.in);
+		int counter=1;
+		while (counter<10) {
+		System.out.println(counter);
+		counter++;
+		}
 	}
 
 }
